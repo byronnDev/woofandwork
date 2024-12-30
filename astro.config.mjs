24d3e3465ea68296549ced71bed299d480b3015d
byronnDev <mikel.e.g.2004@gmail.com>
2024-12-31 00:07:55 +0100

    Add: integrar el soporte para sitemap y crear archivo robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import netlify from '@astrojs/netlify';
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  site: 'https://www.marketinganimales.com',
+  integrations: [sitemap()],
   adapter: netlify(),
   vite: {
     plugins: [
